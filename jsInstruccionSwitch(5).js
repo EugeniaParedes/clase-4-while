bd7442fa0c8ab53499df272e0199013a07a379c2
EugeniaParedes <eugenia.paredes182@gmail.com>
2020-02-14 12:24:27 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(5).js
@@ -0,0 +1,24 @@
+function mostrar()
+{
+var hora;
+var laHora;
+
+hora = document.getElementById('hora').value;
+laHora = parseInt(hora);
+
+switch (laHora)
+{
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        alert ("Es de mañana");
+        break;
+}
+
+	
+
+
+
+}//FIN DE LA FUNCIÓN
